682734119e1a0c77f3df402bd958a59933a3b0e0
shhhhhh14 <shauna.dowling1@gmail.com>
2014-11-03 23:02:32 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,2 +1,20 @@
-class main
+class Main
+  require_relative 'Clock'
+  require_relative 'AlarmClock'
+
+  file = File.open("time.txt", "r")
+  hours =  file.readline.chomp
+  minutes = file.readline.chomp
+  seconds = file.readline.chomp
+
+  time = Clock.new(line[0],line[1],line[2])
+  puts time.to_s
+
+  i= 0
+  until i == 10 do
+    seconds = clock.tick!(i)
+
+    time_array = clock.now(hours, minutes, seconds)
+    i += 1
+  end
 end

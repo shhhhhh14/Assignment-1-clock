1b94cd9376d0ce982bafb1047b473001066f7dd1
shhhhhh14 <shauna.dowling1@gmail.com>
2014-11-03 23:03:08 +0000

    Update clock.rb

--- clock.rb
+++ clock.rb
@@ -1,2 +1,30 @@
-class clock
+class Clock
+  attr_accessor :hours, :minutes, :seconds
+
+  def initialize(hours, minutes,seconds)
+    @hours = hours.to_s
+    @minutes = minutes.to_s
+    @seconds = seconds.to_s
+  end
+
+  def to_s
+    puts "#{@hours}:#{@minutes}:#{@seconds}"
+  end
+
+  def tick
+    @seconds += 1
+    case
+      when @seconds > 59
+        @minutes += 1
+        @seconds = 0
+      when @minutes > 59
+        @hours += 1
+        @minutes = 0
+      when @hours > 23
+        @hours = 0
+    end
+  end
+
+
+
 end
